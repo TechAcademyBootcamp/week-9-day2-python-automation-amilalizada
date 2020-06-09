f73e7df5194c6f1ed5524f97bfeed466a20cc91f
amilalizada <elizade_amil@inbox.ru>
2020-06-09 20:10:02 +0400

    task_3

--- exceptions/unsolved.py
+++ exceptions/unsolved.py
@@ -1,12 +1,22 @@
 def parse_input(user_input):
-    input_list = user_input.split()
-    ## write codes and exceptions
-    ## this function must return number1, operation, number2
+
+  input_list = user_input.split(' ')
+  ## write codes and exceptions
+  if len(input_list)!=3:
+    raise Exception('saydan artiqdir.')
+  try:
+    n1 = float(input_list[0])
+    n2 = float(input_list[2])
+  
+  except:
+    raise Exception('error')
+  ## this function must return number1, operation, number2
+  return n1 , input_list[1] , n2
 
 
 
 def calculate(n1, op, n2):
-
+  
   if op == '+':
     return n1 + n2
   if op == '-':
@@ -16,6 +26,8 @@ def calculate(n1, op, n2):
   if op == '/':
     return n1 / n2
   #write exception
+  
+  raise Exception('2-ci arqument qebul olunan deil')
 
 
 while True:
